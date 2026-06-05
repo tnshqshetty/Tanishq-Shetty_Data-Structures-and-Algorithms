c59758464eadaad5549944cd3905d689ba5927c3
Tanishq Shetty <tnshqshetty@gmail.com>
2026-06-05 19:33:02 +0530

    Add first DSA solutions

---
+++ If.java
@@ -0,0 +1,3 @@
+public class If-Else {
+    
+}
---
+++ InputOutput.java
@@ -0,0 +1,17 @@
+ import java.util.*;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        sc.close();
+    }
+}
+
+/*Complexity Analysis:
+Time Complexity: O(1)  as the program carries out a single constant-time task, namely printing a fixed, predetermined number ,time and operations is constant
+
+Space Complexity: O(1)  because no extra space was used.*/
+
+
